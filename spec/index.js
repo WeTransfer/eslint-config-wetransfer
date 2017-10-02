2d626122f12bd6712e93638d24d2e9f6f4d6699b
Sebass van Boxel <sebass@wetransfer.com>
2017-10-02 12:29:09 +0200

    Added lockFile, tests

---
+++ spec/index.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const eslint = require('eslint');
+const config = require('../');
+
+// The source files to test.
+const repoFiles = [
+  'index.js',
+  'spec/index.js'
+];
+
+// esLint options
+const eslintOpts = {
+  useEslintrc: false,
+  envs: ['node', 'es6'],
+  parserOptions: {ecmaVersion: 2017},
+  rules: config.rules,
+};
+
+// Run linter
+const report = new eslint.CLIEngine(eslintOpts).executeOnFiles(repoFiles);
+
+// Check for errors and warnings
+assert.equal(report.errorCount, 0);
+assert.equal(report.warningCount, 0);
