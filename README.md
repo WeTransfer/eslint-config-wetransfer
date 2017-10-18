81f5dfcf4244a317d273ec60fc4e61255601a945
Arkaitz Garro <arkaitz.garro@gmail.com>
2017-10-18 12:33:04 +0200

    Docs: add rules explanation

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ It is eslint@4+-compatible and maintained by the friendly folks at WeTransfer.
 ## Installation 
 1. Install this config package and ESLint: `$ npm install --save-dev eslint eslint-config-wetransfer`
 2. add `"extends": "wetransfer"` to your .eslintrc
+
+## Which rules do we define?
+Please take a look to the source code of [index.js](https://github.com/WeTransfer/eslint-config-wetransfer/blob/master/index.js), which lists every ESLint rule provided by this configuration.
--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 module.exports = {
   'rules': {
-    // Indendation and line-breaks
+    // Two spaces indentation
     'indent': [
       'error',
       2,
       {
+        // Indentation level for case clauses in switch statements
         'SwitchCase': 1
       }
     ],
+
+    // Unix style line breaks
     'linebreak-style': [
       'error',
       'unix'
     ],
 
-    // Be consistent when using quotes, and when using them, use single quotes.
+    // Be consistent when using quotes on property names
     'quote-props': [
       'error',
       'consistent'
     ],
+
+    // Use single quotes
     'quotes': [
       'error',
       'single'
@@ -29,22 +34,46 @@ module.exports = {
       'always'
     ],
 
-    // Function and keyword styling
-    'space-before-function-paren': ['error',
+    // Spaces between functions and parenthesis
+    'space-before-function-paren': [
+      'error',
       {
         'anonymous': 'never',
         'named': 'never',
         'asyncArrow': 'always'
       }
     ],
-    'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
-    'no-confusing-arrow': 'error',
-    'arrow-spacing': 'error',
-    'keyword-spacing': 'error',
-    'arrow-parens': 'error',
-    'no-param-reassign': [ 'off', { 'props': true } ],
 
-    // Use type-safe equality operators
+    // Function declaration styles
+    'func-style': [
+      'error',
+      'declaration',
+      {
+        'allowArrowFunctions': true
+      }
+    ],
+
+    // Confusion with comparisons and arrow functions
+    'no-confusing-arrow': 'error',
+
+    // Spaces for arrow functions
+    'arrow-spacing': 'error',
+
+    // Spaces for keywords
+    'keyword-spacing': 'error',
+
+    // Arrow function arguments parenthesis
+    'arrow-parens': 'error',
+
+    // Disallow reassignment of function parameters
+    'no-param-reassign': [
+      'off',
+      {
+        'props': true
+      }
+    ],
+
+    // Use type-safe equality operators.
     'eqeqeq': [
       'error',
       'always'
@@ -52,31 +81,61 @@ module.exports = {
 
     // Prevent typing useless code
     'no-unused-vars': 'error',
+
+    // Unnecessary return in else block
     'no-else-return': 'error',
+
+    // Prevent empty functions
     'no-empty-function': 'error',
+
+    // Prevent unnecessary nested blocks
     'no-lone-blocks': 'error',
+
+    // Poor lonely if
     'no-lonely-if': 'error',
+
+    // Disallow multiple spaces
     'no-multi-spaces': 'error',
+
+    // Disallow expressions with no effect
     'no-unused-expressions': [
       'error',
       {
         'allowShortCircuit': true
       }
     ],
+
+    // Disallow early use of variables and functions
     'no-use-before-define': 'error',
+
+    // Disallow unnecessary constructor
     'no-useless-constructor': 'error',
+
+    // Suggest using const
     'prefer-const': 'error',
+
+    // Use spread operator over `.apply()`
     'prefer-spread': 'error',
 
-    // Other styling rules
+    // Consistent brace style for blocks.
     'brace-style': 'error',
+
+    // Disallow trailing commas
     'comma-dangle': [
       'error',
       'never'
     ],
+
+    // Disallow Use of Alert
     'no-alert': 'error',
+
+    // Allow usesless espaces
     'no-useless-escape': 'off',
+
+    // No explanation needed.
     'no-trailing-spaces': 'error',
+
+    // Only one empty line please
     'no-multiple-empty-lines': [
       'error',
       {
@@ -85,11 +144,23 @@ module.exports = {
         'maxBOF': 0
       }
     ],
+
+    // No yoda conditions. I think we want the opposite.
     'yoda': 'error',
+
+    // Require curly braces
     'curly': [ 'error', 'all' ],
+
+    // Disallow functions in loops
     'no-loop-func': 'error',
+
+    // Disallow multiline strings
     'no-multi-str': 'error',
+
+    // Disallow assignment in return statement
     'no-return-assign': 'error',
+
+    // Require variable declarations to be at the top of their scope
     'vars-on-top': 'error'
   }
 };
