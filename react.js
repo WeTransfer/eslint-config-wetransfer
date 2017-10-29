bdefa350fa644b247e9afef056935ef53d871646
Arkaitz Garro <arkaitz.garro@gmail.com>
2017-10-29 09:45:23 +0100

    feat: Add React specific rules (#15)

---
+++ react.js
@@ -0,0 +1,10 @@
+module.exports = {
+  rules: {
+    'react/no-find-dom-node': 'error',
+    'react/no-danger': ['warn'],
+    'react/no-deprecated': ['error'],
+    'react/no-string-refs': ['error'],
+    'react/no-unescaped-entities': ['warn'],
+    'react/jsx-indent': [2, 2],
+  }
+};
